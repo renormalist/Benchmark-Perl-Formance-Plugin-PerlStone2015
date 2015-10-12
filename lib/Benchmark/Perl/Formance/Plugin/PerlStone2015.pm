4ad3e9c578057800d6b94ed8c757683626bf62f9
Steffen Schwigon <ss5@renormalist.net>
2015-10-12 22:48:20 +0200

    find sub results
    ...which may be arbitrary deep down the
    structure, so we search Benchmark keys and
    iterate up the tree again.

--- lib/Benchmark/Perl/Formance/Plugin/PerlStone2015.pm
+++ lib/Benchmark/Perl/Formance/Plugin/PerlStone2015.pm
@@ -5,11 +5,7 @@ use warnings;
 package Benchmark::Perl::Formance::Plugin::PerlStone2015;
 # ABSTRACT: Benchmark::Perl::Formance plugin covering a representative set of sub benchmarks
 
-#############################################################
-#                                                           #
-# Benchmark Code ahead - Don't touch without strong reason! #
-#                                                           #
-#############################################################
+use Data::DPath 'dpath', 'dpathi';
 
 sub perlstone
 {
@@ -52,6 +48,27 @@ sub perlstone
                 }
         }
         return \%results;
+}
+
+sub _find_sub_results
+{
+        my ($RESULTS) = @_;
+
+        my %sub_results = ();
+
+        my $benchmarks = dpathi($RESULTS)->isearch("//Benchmark");
+        while ($benchmarks->isnt_exhausted) {
+                my @keys;
+                my $benchmark = $benchmarks->value;
+                my $ancestors = $benchmark->isearch ("/::ancestor");
+
+                while ($ancestors->isnt_exhausted) {
+                        my $key = $ancestors->value->first_point->{attrs}{key};
+                        push @keys, $key if defined $key;
+                }
+                $sub_results{join(".", reverse @keys)} = ${$benchmark->first_point->{ref}}->{Benchmark}[0];
+        }
+        return \%sub_results;
 }
 
 sub _aggregations
